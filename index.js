01f6f889580190b5572161cf6052378a2b81e291
wenlileong-dev <wenlileong98@gmail.com>
2021-08-22 19:48:47 +1000

    test skeleton code

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
+//connect with database
 require("./models/db");
+
+//require router
 const templateRouter = require("./routes/templateRouter");
 
 let port = process.env.PORT || 5000;
-
 const app = express();
-
 app.use(cors());
 app.use(express.json({ urlencoded: true }));
 app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+//routing
 app.use("/api", templateRouter);
 
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
-//bug fix testing
 
 app.listen(port, () => {
   console.log(`The server is runnning at port ${port}`);
