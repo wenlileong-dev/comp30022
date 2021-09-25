84bca27621fe751fff6a5bafd4c5ac4c8b9bbf73
wenlileong-dev <wenlileong98@gmail.com>
2021-09-25 18:43:52 +1000

    auth route user

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,14 +1,18 @@
-import * as React from 'react';
+import * as React from "react";
 import { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 // import axios from '../commons/axios.js';
 import axios from "axios";
 import { Link } from "react-router-dom";
+import AuthFail from "./../components/AuthFail";
 // import { useHistory } from "react-router-dom";
 
 function Account() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [authFailMsg, setAuthFailMsg] = useState("");
+
   // let history = useHistory()
   const logoutUser = async () => {
     let result = await axios.post("/user/logout");
@@ -26,21 +30,21 @@ function Account() {
   const [lastName, setLastName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   useEffect(() => {
-    
-      getUserDetails();
-      console.log(email)
-    
+    getUserDetails();
+    console.log(email);
   }, []);
 
   const getUserDetails = () => {
     axios
       .get(`/user/`)
       .then((response) => {
-        console.log(response.data)
+        console.log(response.data);
         if (!response.data.success) {
-          alert(response.data.errorMsg);
+          setIsAuth(false);
+          setAuthFailMsg(response.data.errorMsg);
           window.location.href = "/login";
         } else {
+          setIsAuth(true);
           setEmail(response.data.user.email);
           setFirstName(response.data.user.firstName);
           setLastName(response.data.user.lastName);
@@ -50,65 +54,68 @@ function Account() {
       .catch((error) => console.error(`Error: ${error}`));
   };
 
-
-  const onEdit = () =>{
+  const onEdit = () => {
     window.location.href = "/user/editInfo";
     // history.push({
     //   pathname: "/user/editInfo", state: {
     //     user: props
     //   }
     // })
-  }
+  };
   return (
     <React.Fragment>
-      <p>Account Page</p>
-      <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      >
-        <div>
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            // defaultValue="123"
-            value={email}
-          />
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            // defaultValue="123"
-            value={firstName}
-          />
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            // defaultValue="123"/
-            value={lastName}
-          />
-          <TextField
-            disabled
-            id="outlined-disabled"
-            label="Disabled"
-            value={phoneNumber}
-            // defaultValue="123"
-          />
-          <Button type="primary" onClick={onEdit}>
-            <Link to="/user/editInfo">
-            Edit
-            </Link>
-            </Button>
-          <Button variant="primary" onClick={logoutUser}>
-            Logout
-          </Button>
-        </div>
-      </Box>
+      {isAuth && (
+        <>
+          <p>Account Page</p>
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Disabled"
+                // defaultValue="123"
+                value={email}
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Disabled"
+                // defaultValue="123"
+                value={firstName}
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Disabled"
+                // defaultValue="123"/
+                value={lastName}
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Disabled"
+                value={phoneNumber}
+                // defaultValue="123"
+              />
+              <Button type="primary" onClick={onEdit}>
+                <Link to="/user/editInfo">Edit</Link>
+              </Button>
+              <Button variant="primary" onClick={logoutUser}>
+                Logout
+              </Button>
+            </div>
+          </Box>
+        </>
+      )}
+
+      {authFailMsg && <AuthFail msg={authFailMsg} />}
       {/* <div style={{width:'40%', margin:'auto'}}>
           <Form form={form} layout="vertical">
             <Form.Item label="Email">
@@ -127,10 +134,10 @@ function Account() {
               <Input placeholder="password" defaultValue={props.location.state.user.password}/>
             </Form.Item>
             <Form.Item> */}
-              {/* <Button type="primary" onClick={onEdit}>Edit</Button> */}
-            {/* </Form.Item>
+      {/* <Button type="primary" onClick={onEdit}>Edit</Button> */}
+      {/* </Form.Item>
           </Form> */}
-        
+
       {/* <Button variant="primary" onClick={logoutUser}>
         Logout
       </Button> */}
