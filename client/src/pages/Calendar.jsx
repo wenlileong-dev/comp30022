2f85f13de26dc2674ae548ed096445985c6d6756
wenlileong-dev <wenlileong98@gmail.com>
2021-09-25 10:53:29 +1000

    with auth fail handler

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -5,38 +5,26 @@ import CalendarTitle from "./../components/Calendar/CalendarTitle";
 import CalendarDays from "./../components/Calendar/CalendarDays";
 import WeeklyCalendar from "../components/Calendar/WeeklyCalendar";
 import mobileView from "../screenSize";
-<<<<<<< HEAD
-=======
 import AuthFail from "./../components/AuthFail";
->>>>>>> feature/handle-authentication-failure
 
 function Calendar() {
   let today = new Date();
   let [month, setMonth] = useState(today.getMonth());
   let [year, setYear] = useState(today.getFullYear());
   const [events, setEvents] = useState([]);
-<<<<<<< HEAD
-=======
   const [isAuth, setIsAuth] = useState(false);
   const [authFailMsg, setAuthFailMsg] = useState("");
->>>>>>> feature/handle-authentication-failure
 
   //fetch the events of the month
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(`/api/calendar/${month}/${year}`);
       if (result.data.status !== 200) {
-<<<<<<< HEAD
-        alert(result.data.errorMsg);
-        window.location.href = "/login";
-      } else {
-=======
         setIsAuth(false);
         setAuthFailMsg(result.data.errorMsg);
         window.location.href = "/login";
       } else {
         setIsAuth(true);
->>>>>>> feature/handle-authentication-failure
         setEvents(result.data.data);
       }
     };
