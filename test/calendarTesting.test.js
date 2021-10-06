9a4ec8206bcf1ce394df3d7ef89c5c4669f3db29
wenlileong-dev <wenlileong98@gmail.com>
2021-10-06 15:46:05 +1100

    test github action token

--- test/calendarTesting.test.js
+++ test/calendarTesting.test.js
@@ -8,8 +8,7 @@ const api = chai.request(app).keepOpen();
 
 describe("Calendar API Testing", () => {
   let token = "";
-  let eventID = "615d2649f7f9674a78d6108f";
-  beforeEach((done) => {
+  before((done) => {
     let user = {
       email: "test@mail.com",
       password: "123qwert",
@@ -25,27 +24,8 @@ describe("Calendar API Testing", () => {
       });
   });
 
-  afterEach((done) => {
-    token = "";
+  it("test github action", (done) => {
+    token.should.have.lengthOf.above(1);
     done();
-  });
-
-  describe("delete event", () => {
-    it("delete event with valid eventID", (done) => {
-      api.delete(`/api/calendar/${eventID}`).end((err, res) => {
-        res.body.status.should.be.eql(200);
-        res.body.data.should.be.a("object");
-        res.body.data._id.should.be.eql(eventID);
-        done();
-      });
-    });
-
-    it("delete event with invalid eventID", (done) => {
-      api.delete("/api/calendar/xxxxx").end((err, res) => {
-        res.body.status.should.be.eql(400);
-        res.body.errorMsg.should.be.eql("Bad Request - Invalid eventID");
-        done();
-      });
-    });
   });
 });
