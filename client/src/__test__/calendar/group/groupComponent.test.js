3f0309d8877e583f15f59a55aea232724aac11f3
zyx366474 <yixzhong@student.unimelb.edu.au>
2021-10-12 20:11:58 +1100

    display remark

---
+++ client/src/__test__/calendar/group/groupComponent.test.js
@@ -0,0 +1,148 @@
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import GroupComponent from "../../components/Group/GroupComponent";
+import CalendarHeader from "../../components/Calendar/CalendarHeader";
+import CalendarDays from "../../components/Calendar/CalendarDays";
+import CalendarDay from "../../components/Calendar/CalendarDay";
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("Testing Group display", () => {
+  let month = 8;
+  let year = 2021;
+  const nextMonth = jest.fn();
+  const prevMonth = jest.fn();
+  let contact = {
+    firstName: 'ContactTest',
+    lastName: 'ContactTest',
+    gender: 'male',
+    email: 'testContact@mail.com',
+    phone: '00000000',
+    department:'',
+    address:'',
+    reamrk:'',
+    groupID: "615300aff44c8461f8432d7f"
+  };
+  test("testing group display correct contacts", () => {
+    render(<GroupComponent contact={contact}/>);
+
+    const groupComponentElement = screen.getByTitle("calendar-title");
+    expect(groupComponentElement).toBeInTheDocument();
+    expect(groupComponentElement).toHaveTextContent("September");
+    expect(groupComponentElement).toHaveTextContent("2021");
+  });
+
+  test("testing next month and previous month function", () => {
+    render(
+      <CalendarTitle
+        month={month}
+        year={year}
+        nextMonth={nextMonth}
+        prevMonth={prevMonth}
+      />
+    );
+    const prevMonthElement = screen.getByTestId("ArrowLeftIcon");
+    const nextMonthElement = screen.getByTestId("ArrowRightIcon");
+    fireEvent.click(prevMonthElement);
+    expect(prevMonth).toHaveBeenCalled();
+    fireEvent.click(nextMonthElement);
+    expect(nextMonth).toHaveBeenCalled();
+  });
+
+  test("render add event form when add new event button is clicked", () => {
+    render(<CalendarTitle month={month} year={year} />);
+    const addNewEventButton = screen.getByRole("button", { name: "New Event" });
+    fireEvent.click(addNewEventButton);
+    const popupElement = screen.getByTestId("modal-popup");
+    const addEventFormElement = screen.getByTestId("add-event-form");
+    expect(popupElement).toBeInTheDocument();
+    expect(addEventFormElement).toBeInTheDocument();
+  });
+});
+
+test("testing calendar header", () => {
+  render(<CalendarHeader />);
+  const calendarHElement = screen.getByTestId("mytestid");
+  expect(calendarHElement).toBeInTheDocument();
+});
+
+describe("show calendar events", () => {
+  let month = 8;
+  let year = 2021;
+  const event = [
+    {
+      people: [],
+      meetingNotes: "",
+      _id: "61457c79f8277f402cd5069f",
+      title: "Testing 1",
+      description: "Description for Testing 1",
+      date: "2021-09-18T00:00:00.000Z",
+      time: "2021-09-17T23:00:00.992Z",
+      eventType: "Offline",
+      location: "CBD",
+      timeInNumbers: 540,
+      id: "61457c79f8277f402cd5069f",
+    },
+    {
+      people: [],
+      meetingNotes: "",
+      _id: "61457c96f8277f402cd506a2",
+      title: "Testing 2",
+      description: "Description for Testing 2",
+      date: "2021-09-18T00:00:00.000Z",
+      time: "2021-09-18T02:00:00.993Z",
+      eventType: "Online",
+      location: "Microsoft Team",
+      timeInNumbers: 720,
+      id: "61457c96f8277f402cd506a2",
+    },
+  ];
+  test("show number of days in a month", async () => {
+    render(
+      <CalendarDays
+        month={month}
+        year={year}
+        events={[...Array(30)].map((e) => [])}
+      />
+    );
+    let additionalSpace = new Date(year, month, 1).getDay();
+    const calendarDayComponenet = screen.getAllByTestId("calendar-a-day");
+    expect(calendarDayComponenet).toHaveLength(30 + additionalSpace);
+  });
+
+  test("show event for a day", () => {
+    render(<CalendarDay day={1} month={month} year={year} event={event} />);
+    const calendarHElement = screen.getByTestId("calendar-a-day");
+    expect(calendarHElement).toBeInTheDocument();
+    expect(calendarHElement).toHaveTextContent(event[0].title);
+    expect(calendarHElement).toHaveTextContent(event[1].title);
+  });
+
+  test("event details popup when the day is clicked", () => {
+    render(<CalendarDay day={1} month={month} year={year} event={event} />);
+    const calendarDayElement = screen.getByTestId("calendar-a-day");
+    fireEvent.click(calendarDayElement);
+    const popupElement = screen.getByTestId("modal-popup");
+    expect(popupElement).toBeInTheDocument();
+    const eventDetailComponent = screen.getAllByTestId("event-day-component");
+    expect(eventDetailComponent).toHaveLength(2);
+    expect(eventDetailComponent[0]).toHaveTextContent(event[0].title);
+    expect(eventDetailComponent[1]).toHaveTextContent(event[1].title);
+  });
+
+  test("render edit event form when edit event button is clicked", () => {
+    render(<CalendarDay day={1} month={month} year={year} event={event} />);
+    const calendarDayElement = screen.getByTestId("calendar-a-day");
+    fireEvent.click(calendarDayElement);
+    const editEventButton = screen.getAllByRole("button", {
+      name: "Edit Event",
+    });
+    fireEvent.click(editEventButton[0]);
+    const editEventFormComponent = screen.getByTestId("edit-event-form");
+    expect(editEventFormComponent).toBeInTheDocument();
+    expect(editEventFormComponent).toHaveTextContent(event[0].title);
+  });
+});
--- client/src/components/Group/GroupComponent.jsx
+++ client/src/components/Group/GroupComponent.jsx
@@ -4,19 +4,50 @@ import { Link, Route } from "react-router-dom";
 
 function GroupComponent(props) {
 
-    return (
-        <div style={{ fontSize: "15px" }}>
-            <Link
-                
-                to={{pathname:`/contact/info`,
-                state:{contact:props.contact}}}
-            >
-            {props.contact.firstName} {props.contact.lastName} 
-            </Link>
-            <br/>
-        </div>
+    if (props.contact.remark) {
+        return (
+            <div style={{ fontSize: "15px" }}>
+                <Link
+                    
+                    to={{pathname:`/contact/info`,
+                    state:{contact:props.contact}}}
+                >
+                {props.contact.firstName} {props.contact.lastName} {"(" + props.contact.remark + ")"}
+                </Link>
+                <br/>
+            </div>
+    
+        );
+    }
+    else {
+        return (
+            <div style={{ fontSize: "15px" }}>
+                <Link
+                    
+                    to={{pathname:`/contact/info`,
+                    state:{contact:props.contact}}}
+                >
+                {props.contact.firstName} {props.contact.lastName} 
+                </Link>
+                <br/>
+            </div>
+    
+        );
+    }
 
-    );
+    // return (
+    //     <div style={{ fontSize: "15px" }}>
+    //         <Link
+                
+    //             to={{pathname:`/contact/info`,
+    //             state:{contact:props.contact}}}
+    //         >
+    //         {props.contact.firstName} {props.contact.lastName} 
+    //         </Link>
+    //         <br/>
+    //     </div>
+
+    // );
 }
 
 export default GroupComponent;
